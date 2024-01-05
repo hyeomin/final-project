7321d7857e3280250c09fb5568917fa6ac2b6444
zerotonine2da <96953412+zerotonine2da@users.noreply.github.com>
2024-01-05 17:41:11 +0900

    add mainSub Page

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,12 @@
-import CS from "./CommonStyle";
-
+import CS from './CommonStyle';
+import MainSub from '../components/mainSub/MainSub';
 function Main() {
-    return <CS.FullContainer>Main</CS.FullContainer>;
+    return (
+        <CS.FullContainer>
+            Main
+            <MainSub />
+        </CS.FullContainer>
+    );
 }
 
 export default Main;
