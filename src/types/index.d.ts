8adabc3ea4cb0225904bf96ae3b85decba4115f3
Solyi Park <itsoholy21@gmail.com>
2024-01-08 20:27:58 +0900

    [add ] merge

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -15,3 +15,16 @@ type Post = {
   profileImg?: string | null;
   coverUrl?: string | null;
 };
+
+export type PostType = {
+  id: string;
+  category: string;
+  title: string;
+  content: string;
+  createdAt: Date;
+  updatedAt: Date;
+  hashtag: null;
+  uid: null;
+  likeCount: number;
+  likedUsers: null;
+};
