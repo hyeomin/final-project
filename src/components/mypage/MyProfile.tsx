58f7bed8bf42b535646a96530ddae52471600d6a
heeneeee <gmldnjd0614@gmail.com>
2024-01-09 18:05:44 +0900

    modify: 회원가입시 프로필 업로드 기능 수정

--- src/components/mypage/MyProfile.tsx
+++ src/components/mypage/MyProfile.tsx
@@ -1,15 +1,41 @@
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import HabitCalendar from './HabitCalendar';
 import LikesPosts from './LikesPosts';
 import MyPosts from './MyPosts';
 import St from './style';
 import defaultImg from '../../assets/defaultImg.jpg';
+import { auth, db } from '../../shared/firebase';
+import { getAuth, updateProfile } from 'firebase/auth';
+import { addDoc, collection } from 'firebase/firestore';
 
 function MyProfile() {
   const [activeTab, setActiveTab] = useState('calendar');
+  const [newDisplayName, setNewDisPlayName] = useState('');
 
   const onClickTabBtn = (name: string) => {
     setActiveTab(name);
+  };
+
+  const onChangeDisplayName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewDisPlayName(e.target.value);
+  };
+
+  const onSubmitModifyProfile = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const auth = getAuth();
+    if (auth.currentUser?.displayName !== newDisplayName) {
+      await updateProfile(auth.currentUser!, {
+        displayName: newDisplayName
+      });
+      const docRef = await addDoc(collection(db, 'users'), {
+        displayName: auth.currentUser?.displayName,
+        profileImg: auth.currentUser?.photoURL,
+        uid: auth.currentUser?.uid,
+        role: 'user'
+      });
+      console.log(auth.currentUser);
+      console.log(auth.currentUser?.displayName);
+    }
   };
 
   return (
@@ -18,9 +44,11 @@ function MyProfile() {
         <St.ProfileEditWrapper>
           ProfileEditWrapper
           <St.UserInfo>
-            <St.MyImage src={defaultImg} alt="defaultImg" />
-            <St.MyNickname>user Nickname</St.MyNickname>
-            <St.EditBtn>수정하기</St.EditBtn>
+            <St.MyImage src={auth.currentUser?.photoURL! || defaultImg} alt="defaultImg" />
+            <St.MyNickname>{auth.currentUser?.displayName}</St.MyNickname>
+            <input type="text" value={newDisplayName} onChange={onChangeDisplayName} />
+            <St.EditBtn onClick={onSubmitModifyProfile}>수정하기</St.EditBtn>
+            <button>완료</button>
           </St.UserInfo>
         </St.ProfileEditWrapper>
         <St.MySectionWrapper>
