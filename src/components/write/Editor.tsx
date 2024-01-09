825d232335ceebc392b22bb016c040d936d9087c
Hyeo Min Park <hyeominpark@Hyeos-MacBook-Air.local>
2024-01-09 15:00:20 +0900

    merge: before solyi merge

---
+++ src/components/write/Editor.tsx
@@ -0,0 +1,89 @@
+import { useMemo, useRef } from 'react';
+import ReactQuill from 'react-quill';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import styled from 'styled-components';
+import { categoryListState, categoryState, contentState, titleState } from '../../recoil/posts';
+import imageHandler from './imageHandler';
+
+function Editor() {
+  const TITLE = 'title';
+
+  const [category, setCategory] = useRecoilState(categoryState);
+  const [title, setTitle] = useRecoilState(titleState);
+  const [content, setContent] = useRecoilState(contentState);
+
+  const categoryList = useRecoilValue(categoryListState);
+
+  const quillRef = useRef<ReactQuill>(null);
+
+  const modules = useMemo(() => {
+    return {
+      toolbar: {
+        container: [
+          [{ header: [1, 2, 3, false] }],
+          ['bold', 'italic', 'underline', 'strike'],
+          ['blockquote'],
+          [{ list: 'ordered' }, { list: 'bullet' }],
+          [{ color: [] }, { background: [] }],
+          [{ align: [] }, 'link', 'image']
+        ],
+        handlers: {
+          image: () => imageHandler(quillRef)
+        }
+      }
+    };
+  }, []);
+
+  const editorStyle = { height: '600px', overflow: 'auto' };
+
+  return (
+    <WritingArea>
+      <select value={category} onChange={(event) => setCategory(event.target.value)}>
+        <option value="" disabled hidden>
+          카테고리
+        </option>
+        {categoryList.map((item) => {
+          return <option key={item.id}>{item.name}</option>;
+        })}
+      </select>
+      <input
+        name={TITLE}
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+        placeholder="제목을 입력하세요."
+      />
+      <EditorContainer>
+        <ReactQuill
+          style={editorStyle}
+          theme="snow"
+          value={content}
+          onChange={setContent}
+          modules={modules}
+          ref={quillRef}
+        />
+      </EditorContainer>
+    </WritingArea>
+  );
+}
+
+export default Editor;
+
+const WritingArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 15px;
+  background-color: white;
+
+  & select {
+    flex: 0;
+  }
+
+  & input {
+    font-size: x-large;
+    padding: 10px;
+  }
+`;
+
+const EditorContainer = styled.div`
+  padding: 10px 0;
+`;
--- src/recoil/posts.ts
+++ src/recoil/posts.ts
@@ -1,7 +1,30 @@
 import { atom } from 'recoil';
+import {
+  EDIT_COVER_IMAGE,
+  EDIT_INPUT_CATEGORY,
+  EDIT_INPUT_CATEGORY_LIST,
+  EDIT_INPUT_CONTENT,
+  EDIT_INPUT_HASTAG,
+  EDIT_INPUT_TITLE
+} from './keys';
 
-const categoryRecoilState = atom({
-  key: 'categoryState',
+const titleState = atom({
+  key: EDIT_INPUT_TITLE,
+  default: ''
+});
+
+const contentState = atom({
+  key: EDIT_INPUT_CONTENT,
+  default: ''
+});
+
+const categoryState = atom({
+  key: EDIT_INPUT_CATEGORY,
+  default: ''
+});
+
+const categoryListState = atom({
+  key: EDIT_INPUT_CATEGORY_LIST,
   default: [
     { id: 0, name: '카테고리 없음', isAdmin: false },
     { id: 1, name: '제품 추천', isAdmin: false },
@@ -12,4 +35,14 @@ const categoryRecoilState = atom({
   ]
 });
 
-export { categoryRecoilState };
+const hashtagState = atom<string[]>({
+  key: EDIT_INPUT_HASTAG,
+  default: []
+});
+
+const coverImageState = atom<File[]>({
+  key: EDIT_COVER_IMAGE,
+  default: []
+});
+
+export { categoryListState, categoryState, contentState, coverImageState, hashtagState, titleState };
