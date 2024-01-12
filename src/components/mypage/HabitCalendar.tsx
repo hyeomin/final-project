d858b8315f8980828a5a15a22d574f7e4eecdb45
heeneeee <gmldnjd0614@gmail.com>
2024-01-12 15:57:50 +0900

    feat: 캘린더 게시물 갯수에 따른 숫자 표시 기능

--- src/components/mypage/HabitCalendar.tsx
+++ src/components/mypage/HabitCalendar.tsx
@@ -29,32 +29,32 @@ const HabitCalendar = ({ date }: any) => {
     queryFn: getMyPosts
   });
   const createdAtList = data ? data.map((data) => getFormattedDateCustom(data.createdAt!)) : [];
-  const dayList = [
+  const dayList: string[] = [
     //테스트용
     '2023. 12. 12.',
     '2023. 12. 25.',
     '2024. 01. 01.',
     '2024. 01. 05.',
     '2024. 01. 07.',
+    '2024. 01. 07.',
     '2024. 01. 12.'
   ];
   console.log('포스트의 날짜 목록 가져오기', createdAtList);
 
-  // const dayList = ['2024-01-01', '2024-01-04', '2024-01-05', '2024-01-09', '2024-01-11'];
-  // // 각 날짜 타일에 컨텐츠 추가
-  // // const addContent = ({ date }: any) => {
-  //   // 해당 날짜(하루)에 추가할 컨텐츠의 배열
-  //   const contents = [];
+  const getElCount = (arr: string[]): Record<string, number> =>
+    arr.reduce((ac: Record<string, number>, v) => {
+      ac[v] = (ac[v] || 0) + 1;
+      return ac;
+    }, {});
+  const aa = getElCount(dayList);
+  console.log('aa', aa);
 
-  //   // date(각 날짜)가  리스트의 날짜와 일치하면 해당 컨텐츠(이모티콘) 추가
-  //   if (dayList.find((day) => day === moment(date).format('YYYY-MM-DD'))) {
-  //     contents.push(
-  //       <React.Fragment >
-  //         <img key={date} src={habitSticker} width="26" height="26" />
-  //       </React.Fragment>
-  //     );
-  //   }
-  // };
+  // 2023. 12. 12. :  1
+  // 2023. 12. 25. :  1
+  // 2024. 01. 01. :  1
+  // 2024. 01. 05. :  1
+  // 2024. 01. 07. :  2
+  // 2024. 01. 12. :  1
 
   return (
     <St.CalendarWrapper>
@@ -68,8 +68,19 @@ const HabitCalendar = ({ date }: any) => {
           // 일요일부터 시작
           calendarType="gregory"
           tileContent={({ date, view }) => {
-            const formattedDate = moment(date).format('YYYY. MM. DD');
+            const formattedDate = moment(date).format('YYYY. MM. DD.');
+
             if (dayList.find((x) => x === moment(date).format('YYYY. MM. DD.'))) {
+              // postCount가 0으로만 나온다.
+              // aa[formattedDate]가 값이 없으니까 => 0으로 된다?
+              // aa에 문제가 있는지?
+              // formattedDate에 문제가 있는지?
+              console.log({
+                formattedDate,
+                finded: dayList.find((x) => x === moment(date).format('YYYY. MM. DD.')),
+                일치하는지: formattedDate === dayList.find((x) => x === moment(date).format('YYYY. MM. DD.'))
+              });
+              const postCount = aa[formattedDate] || 0;
               return (
                 <>
                   <div className="habitDayContainer" key={formattedDate}></div>
@@ -79,7 +90,8 @@ const HabitCalendar = ({ date }: any) => {
                     src={habitSticker}
                     alt={`habit-sticker-${formattedDate}`}
                   />
-                  {/* <div className="habitDate">{moment(date).format('DD')}</div> */}
+                  x {postCount}
+                  {/* <div clasName="habitDate">{moment(date).format('DD')}</div> */}
                 </>
               );
             }
