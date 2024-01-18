c2a966520e77aaa1d2385b213119f07231da2798
Solyi Park <itsoholy21@gmail.com>
2024-01-18 19:22:32 +0900

    feat: Protected Route 설정완료

---
+++ src/shared/ProtectedRoute.tsx
@@ -0,0 +1,13 @@
+import { useContext } from 'react';
+import { Navigate, Outlet, RouteProps } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
+import { auth } from './firebase';
+
+const ProtectedRoute = () => {
+  const auth = useContext(AuthContext);
+  console.log('로그인상태=====>', auth?.isAuth);
+  if (!auth?.isAuth) return <Navigate to={'/'} replace />;
+  return <Outlet />;
+};
+
+export default ProtectedRoute;
