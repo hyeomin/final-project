95aba8867ca5b9691a6afbbfc5113547b26a54b0
zerotonine2da <96953412+zerotonine2da@users.noreply.github.com>
2024-01-19 11:00:30 +0900

    merge: jin-young help

--- src/components/mypage/LikesPosts.tsx
+++ src/components/mypage/LikesPosts.tsx
@@ -4,122 +4,21 @@ import { useNavigate } from 'react-router-dom';
 import { getAllUsers } from '../../api/authApi';
 import { downloadImageURL, getAdminContents, getUserContents } from '../../api/homeApi';
 import { getLikePosts } from '../../api/myPostAPI';
-import defaultCover from '../../assets/defaultCoverImg.jpeg';
-import defaultProfile from '../../assets/defaultImg.jpg';
-import { QUERY_KEYS } from '../../query/keys';
-import { auth } from '../../shared/firebase';
-import { getFormattedDate_yymmdd } from '../../util/formattedDateAndTime';
-import PostContentPreview from '../common/PostContentPreview';
 import Cs from '../viewAll/style';
-import St from './style';
+import PostCard from './PostCard/PostCard';
+
 const LikesPosts = () => {
-  // 게시물 이동을 위해 Ashley 추가
-  const navigate = useNavigate();
-
-  //포스트 작가 정보 가져오기 위해 Ashley 추가
-  const { data: userList } = useQuery({
-    queryKey: [QUERY_KEYS.USERS],
-    queryFn: getAllUsers
-  });
-
   const { data: likePosts } = useQuery({
-    queryKey: [QUERY_KEYS.POSTS],
+    queryKey: ['posts', { likedPosts: true }],
     queryFn: getLikePosts
   });
-  console.log('이거이거이거 ===>', likePosts);
-
-  const postQueries = useQueries({
-    queries: [
-      {
-        queryKey: ['adminContents'],
-        queryFn: getAdminContents
-      },
-      {
-        queryKey: [QUERY_KEYS.USERPOSTS],
-        queryFn: getUserContents
-      }
-    ]
-  });
-
-  //필터된 posts 목록 (망고관리자 게시물은 임시로 둔다.)
-  // const createdByMango = postQueries[0].data || [];
-  const myPosts = postQueries[1].data || [];
-
-  // 이미지URL 불러오기
-  const imageQueries = useQueries({
-    queries:
-      likePosts?.map((post) => ({
-        queryKey: ['imageURL', post.id],
-        queryFn: () => downloadImageURL(post.id as string)
-      })) || []
-  });
-
-  function removeImageTags(htmlContent: string) {
-    return htmlContent.replace(/<img[^>]*>|<p[^>]*>(?:\s*<br[^>]*>\s*|)\s*<\/p>/g, '');
-  }
 
   return (
-    <St.PostsWrapper>
-      {/* <St.PostsBox>
-        {likePosts?.map((item, idx) => {
-          const imageQuery = imageQueries[idx];
-          return (
-            <Link to={`/detail/${item.id}`}>
-              <St.TextBox>
-                <St.PostImg src={imageQuery.data!} />
-                <St.PostTitle>{item.title}</St.PostTitle>
-                <St.Contents dangerouslySetInnerHTML={{ __html: removeImageTags(item?.content || '') }} />
-              </St.TextBox>
-            </Link>
-          );
-        })}
-      </St.PostsBox> */}
-      <Cs.Contents>
-        {likePosts?.map((post, idx) => {
-          const imageQuery = imageQueries[idx];
-          return (
-            <Cs.Content onClick={() => navigate(`/detail/${post.id}`)}>
-              <Cs.ContentImg src={imageQuery.data || defaultCover} />
-              <Cs.PostInfoContainer>
-                <Cs.UserProfile>
-                  <div>
-                    <Cs.ProfileImg src={auth.currentUser?.photoURL || defaultProfile} alt="profile" />
-                    <Cs.Row>
-                      <p>{userList?.find((user) => user.uid === post.uid)?.displayName}</p>
-                      <span>{getFormattedDate_yymmdd(post.createdAt!)}</span>
-                    </Cs.Row>
-                  </div>
-                  <Cs.HeartClickButton>
-                    <GoHeart />
-                  </Cs.HeartClickButton>
-                </Cs.UserProfile>
-                <Cs.TitleAndContent>
-                  <p>{post.title}</p>
-                  {post.content && <PostContentPreview postContent={post.content} />}
-                  {/* <div
-                      dangerouslySetInnerHTML={{ __html: reduceContent(removeImageTags(post?.content || ''), 41) }}
-                    /> */}
-                </Cs.TitleAndContent>
-                <Cs.CommentAndLikes>
-                  <span>
-                    <GoEye />
-                    {post.viewCount}
-                  </span>
-                  <span>
-                    <GoHeart />
-                    {post.likeCount}
-                  </span>
-                  <span>
-                    <GoComment />
-                    {post.commentCount ?? 0}
-                  </span>
-                </Cs.CommentAndLikes>
-              </Cs.PostInfoContainer>
-            </Cs.Content>
-          );
-        })}
-      </Cs.Contents>
-    </St.PostsWrapper>
+    <Cs.Contents>
+      {likePosts?.map((post) => {
+        return <PostCard key={post.id} post={post} />;
+      })}
+    </Cs.Contents>
   );
 };
 
---
+++ src/components/mypage/PostCard/PostCard.tsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import Cs from './PostCard.styled';
+import { useNavigate } from 'react-router-dom';
+import { getFormattedDate_yymmdd } from '../../../util/formattedDateAndTime';
+import { GoComment, GoEye, GoHeart, GoHeartFill } from 'react-icons/go';
+import PostContentPreview from '../../common/PostContentPreview';
+import { auth, db } from '../../../shared/firebase';
+import defaultProfile from '../../../assets/defaultImg.jpg';
+import defaultCover from '../../../assets/defaultCoverImg.jpeg';
+import { arrayRemove, arrayUnion, doc, updateDoc } from 'firebase/firestore';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { produce } from 'immer';
+
+interface PostCardProps {
+  post: PostType;
+}
+
+function PostCard({ post }: PostCardProps) {
+  const navigate = useNavigate();
+  const currentUserId = auth.currentUser!.uid;
+  const queryClient = useQueryClient();
+
+  const { mutateAsync: toggleLike } = useMutation({
+    mutationFn: async (postId: string) => {
+      const postRef = doc(db, 'posts', postId);
+
+      await updateDoc(postRef, {
+        likedUsers: post.isLiked ? arrayRemove(currentUserId) : arrayUnion(currentUserId)
+      });
+    },
+    onMutate: async (postId) => {
+      queryClient.setQueriesData<PostType[]>({ queryKey: ['posts'] }, (prevPosts) => {
+        if (!prevPosts) return [];
+
+        const nextPosts = produce(prevPosts, (draftPosts) => {
+          const post = draftPosts.find((post) => post.id === postId);
+          if (!post) return draftPosts;
+
+          post.isLiked = !post.isLiked;
+
+          return draftPosts;
+        });
+
+        return nextPosts;
+      });
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['posts'] });
+    }
+  });
+
+  const handleClickLikeButton: React.MouseEventHandler = async (e) => {
+    e.stopPropagation();
+
+    await toggleLike(post.id);
+  };
+
+  return (
+    <Cs.Content onClick={() => navigate(`/detail/${post.id}`)}>
+      <Cs.ContentImg src={defaultCover} />
+      {/* <Cs.ContentImg src={imageQuery.data || defaultCover} /> */}
+      <Cs.PostInfoContainer>
+        <Cs.UserProfile>
+          <div>
+            <Cs.ProfileImg src={auth.currentUser?.photoURL || defaultProfile} alt="profile" />
+            <Cs.Row>
+              {/* <p>{userList?.find((user) => user.uid === post.uid)?.displayName}</p> */}
+              <p>김혜민</p>
+              <span>{getFormattedDate_yymmdd(post.createdAt!)}</span>
+            </Cs.Row>
+          </div>
+          <Cs.HeartClickButton onClick={handleClickLikeButton} $isLiked={!!post.isLiked}>
+            {post.isLiked ? <GoHeartFill /> : <GoHeart />}
+          </Cs.HeartClickButton>
+        </Cs.UserProfile>
+
+        <Cs.TitleAndContent>
+          <p>{post.title}</p>
+          {post.content && <PostContentPreview postContent={post.content} />}
+        </Cs.TitleAndContent>
+
+        <Cs.CommentAndLikes>
+          <span>
+            <GoEye />
+            {post.viewCount}
+          </span>
+          <span>
+            <GoHeart />
+            {post.likeCount}
+          </span>
+          <span>
+            <GoComment />
+            {post.commentCount ?? 0}
+          </span>
+        </Cs.CommentAndLikes>
+      </Cs.PostInfoContainer>
+    </Cs.Content>
+  );
+}
+
+export default PostCard;
