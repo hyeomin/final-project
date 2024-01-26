ed39792f3a0506830d3e58e9102e94469d355c32
Solyi Park <itsoholy21@gmail.com>
2024-01-26 16:52:31 +0900

    modify: 상세페이지- 게시물별 동적OG 메타태그 생성

--- src/hooks/useLikeButton.ts
+++ src/hooks/useLikeButton.ts
@@ -10,10 +10,7 @@ export const useLikeButton = () => {
   const onClickLikeButton = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>, id: string | undefined) => {
     e.preventDefault();
     e.stopPropagation();
-    // if (!currentUser) {
-    //   const confirmation = window.confirm('로그인이 필요합니다. 로그인 페이지로 이동하시겠습니까?');
-    //   if (confirmation) navigate('/auth');
-    // }
+
     if (!currentUser) {
       const onClickCancel = () => {
         modal.close();
