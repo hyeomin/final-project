379ccfd51132e90acddfc2ba7dca56a228828b67
heeneeee <gmldnjd0614@gmail.com>
2024-01-29 21:47:09 +0900

    style: calendar css 수정

--- src/shared/firebase.ts
+++ src/shared/firebase.ts
@@ -1,8 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from '@firebase/firestore';
+import { getFirestore, initializeFirestore } from '@firebase/firestore';
 import { getStorage } from 'firebase/storage';
-
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -13,8 +12,14 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
+// export const app = initializeApp(firebaseConfig);
+// export const auth = getAuth(app);
+// export const db = initializeFirestore(app, {
+//   experimentalForceLongPolling: true
+// });
+
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export default app;
 export const db = getFirestore(app);
