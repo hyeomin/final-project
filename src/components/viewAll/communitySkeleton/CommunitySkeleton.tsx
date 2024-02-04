6925245ceebcfec4649e81317859fb2e397b7375
Solyi Park <itsoholy21@gmail.com>
2024-02-04 22:48:37 +0900

    modify: background-color 제거 / 커뮤니티 미리보기 갯수 8->12

--- src/components/viewAll/communitySkeleton/CommunitySkeleton.tsx
+++ src/components/viewAll/communitySkeleton/CommunitySkeleton.tsx
@@ -141,6 +141,74 @@ const CommunitySkeleton = () => {
             <div className="community-skeleton-counts"></div>
           </div>
         </div>
+        <div className="community-skeleton-card">
+          <div className="community-skeleton-coverImg"></div>
+          <div className="community-skeleton-infoWrapper">
+            <div className="community-skeleton-infoHeader">
+              <div className="community-skeleton-profile"></div>
+              <div className="community-skeleton-text">
+                <p className="community-skeleton-displayName"></p>
+                <p className="community-skeleton-dateAndTime"></p>
+              </div>
+            </div>
+            <div className="community-skeleton-contentWrapper">
+              <p className="community-skeleton-content"></p>
+              <p className="community-skeleton-content"></p>
+            </div>
+            <div className="community-skeleton-counts"></div>
+          </div>
+        </div>
+        <div className="community-skeleton-card">
+          <div className="community-skeleton-coverImg"></div>
+          <div className="community-skeleton-infoWrapper">
+            <div className="community-skeleton-infoHeader">
+              <div className="community-skeleton-profile"></div>
+              <div className="community-skeleton-text">
+                <p className="community-skeleton-displayName"></p>
+                <p className="community-skeleton-dateAndTime"></p>
+              </div>
+            </div>
+            <div className="community-skeleton-contentWrapper">
+              <p className="community-skeleton-content"></p>
+              <p className="community-skeleton-content"></p>
+            </div>
+            <div className="community-skeleton-counts"></div>
+          </div>
+        </div>
+        <div className="community-skeleton-card">
+          <div className="community-skeleton-coverImg"></div>
+          <div className="community-skeleton-infoWrapper">
+            <div className="community-skeleton-infoHeader">
+              <div className="community-skeleton-profile"></div>
+              <div className="community-skeleton-text">
+                <p className="community-skeleton-displayName"></p>
+                <p className="community-skeleton-dateAndTime"></p>
+              </div>
+            </div>
+            <div className="community-skeleton-contentWrapper">
+              <p className="community-skeleton-content"></p>
+              <p className="community-skeleton-content"></p>
+            </div>
+            <div className="community-skeleton-counts"></div>
+          </div>
+        </div>
+        <div className="community-skeleton-card">
+          <div className="community-skeleton-coverImg"></div>
+          <div className="community-skeleton-infoWrapper">
+            <div className="community-skeleton-infoHeader">
+              <div className="community-skeleton-profile"></div>
+              <div className="community-skeleton-text">
+                <p className="community-skeleton-displayName"></p>
+                <p className="community-skeleton-dateAndTime"></p>
+              </div>
+            </div>
+            <div className="community-skeleton-contentWrapper">
+              <p className="community-skeleton-content"></p>
+              <p className="community-skeleton-content"></p>
+            </div>
+            <div className="community-skeleton-counts"></div>
+          </div>
+        </div>
       </div>
     </>
   );
