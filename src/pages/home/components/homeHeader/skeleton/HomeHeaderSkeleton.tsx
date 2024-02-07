bafb7fcde15ade1aab5357fd41253eeab5177818
Solyi Park <itsoholy21@gmail.com>
2024-02-07 12:15:59 +0900

    modify: homeHeader skeleton css 수정

--- src/pages/home/components/homeHeader/skeleton/HomeHeaderSkeleton.tsx
+++ src/pages/home/components/homeHeader/skeleton/HomeHeaderSkeleton.tsx
@@ -2,15 +2,21 @@ import './homeHeaderSkeleton.css';
 
 const HomeHeaderSkeleton = () => {
   return (
-    <>
+    <div className="skeleton-box">
       <div className="skeleton-container">
-        <h2 className="skeleton-h2"></h2>
-        <h1 className="skeleton-h1"></h1>
-        <p className="skeleton-description"></p>
-        <div className="skeleton-button"></div>
-        <div className="skeleton-slide"></div>
+        <div>
+          <div className="skeleton-headerText">
+            <h2 className="skeleton-h2"></h2>
+            <h1 className="skeleton-h1"></h1>
+            <p className="skeleton-description"></p>
+          </div>
+          <div>
+            <div className="skeleton-button"></div>
+            <div className="skeleton-slide"></div>
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
